9b855a78bb3ec3a34f136163168ea4160cfc2905
Ataide Neto <ataide.neto31@gmail.com>
2020-09-19 20:58:43 -0300

    fix rubocop issues

--- spec/business/tweet_business/create_tweet_spec.rb
+++ spec/business/tweet_business/create_tweet_spec.rb
@@ -4,25 +4,25 @@ require 'rails_helper'
 
 RSpec.describe TweetBusiness::CreateTweet do
   let!(:user) { create(:user) }
-  context "when tweet params are right" do
+  context 'when tweet params are right' do
     subject { described_class.new(tweet_params) }
     let(:tweet_params) { attributes_for(:tweet, user_id: user.id) }
 
-    it "creates a tweet" do
+    it 'creates a tweet' do
       expect { subject }.to change {
         Tweet.all.count
       }.from(0).to(1)
     end
 
-    it ".result is tweet object" do
+    it '.result is tweet object' do
       expect(subject.result).to be_an Tweet
     end
 
-    it ".result tweet has tweet params" do
+    it '.result tweet has tweet params' do
       expect(subject.result).to have_attributes(tweet_params)
     end
 
-    it ".result tweet has is the same on database" do
+    it '.result tweet has is the same on database' do
       result = subject.result
       tweet = user.tweets.first
       expect(result.id).to eq(tweet.id)
@@ -31,60 +31,59 @@ RSpec.describe TweetBusiness::CreateTweet do
       expect(result.user_id).to eq(tweet.user_id)
     end
 
-    it ".result is tweet params" do
+    it '.result is tweet params' do
       expect(subject.result).to have_attributes(tweet_params)
     end
 
-    it ".success? is true" do
+    it '.success? is true' do
       expect(subject.success?).to be_truthy
     end
 
-    it ".errors is nil" do
+    it '.errors is nil' do
       expect(subject.errors).to be_falsey
     end
   end
 
-  shared_examples_for "not creates a tweet" do
-    it "not change tweet count" do
-      expect { subject }.to_not change { Tweet.all.count }
+  shared_examples_for 'not creates a tweet' do
+    it 'not change tweet count' do
+      expect { subject }.to_not(change { Tweet.all.count })
     end
 
-    it "not succeed" do
+    it 'not succeed' do
       expect(subject.success?).to be_falsey
     end
 
-    it "have errors" do
+    it 'have errors' do
       expect(subject.errors).to be_truthy
     end
   end
 
-  context "when twitter params are wrong" do
+  context 'when twitter params are wrong' do
     subject { described_class.new(tweet_params) }
 
-    context "blank content" do
-      let(:tweet_params) { {content: "", user_id: 1 } }
-      it_behaves_like "not creates a tweet"
+    context 'blank content' do
+      let(:tweet_params) { { content: '', user_id: 1 } }
+      it_behaves_like 'not creates a tweet'
     end
 
-    context "too short content" do
-      let(:tweet_params) { {content: "..", user_id: 1 } }
-      it_behaves_like "not creates a tweet"
+    context 'too short content' do
+      let(:tweet_params) { { content: '..', user_id: 1 } }
+      it_behaves_like 'not creates a tweet'
     end
 
-    context "too long content" do
-      let(:tweet_params) { {content: Faker::Lorem.characters(number: 280), user_id: 1 } }
-      it_behaves_like "not creates a tweet"
+    context 'too long content' do
+      let(:tweet_params) { { content: Faker::Lorem.characters(number: 280), user_id: 1 } }
+      it_behaves_like 'not creates a tweet'
     end
 
-    context "user not found" do
-      let(:tweet_params) { {content: "tweet", user_id: 1 } }
-      it_behaves_like "not creates a tweet"
+    context 'user not found' do
+      let(:tweet_params) { { content: 'tweet', user_id: 1 } }
+      it_behaves_like 'not creates a tweet'
     end
 
-    context "blank params" do
+    context 'blank params' do
       let(:tweet_params) { {} }
-      it_behaves_like "not creates a tweet"
+      it_behaves_like 'not creates a tweet'
     end
   end
-
 end
--- spec/business/tweet_business/tweet_feed_spec.rb
+++ spec/business/tweet_business/tweet_feed_spec.rb
@@ -12,34 +12,34 @@ RSpec.describe TweetBusiness::TweetFeed do
     create_list(:tweet, 5, user: user_followings.last.following)
   end
 
-  context "tweet feed from user" do
+  context 'tweet feed from user' do
     subject { described_class.new(user.id) }
 
-    it ".result is an Array" do
+    it '.result is an Array' do
       expect(subject.result).to be_an ActiveRecord::Relation
     end
 
-    it "list all tweets from himself and his followings" do
+    it 'list all tweets from himself and his followings' do
       expect(subject.result.size).to eq(15)
     end
 
-    context "after unfollow a user" do
+    context 'after unfollow a user' do
       before { user_followings.first.destroy! }
-      it "dont list user tweets after" do
+      it 'dont list user tweets after' do
         expect(subject.result.size).to eq(10)
       end
     end
 
-    context "after unfollow all users" do
+    context 'after unfollow all users' do
       before { user.user_followings.destroy_all }
-      it "dont list users tweets after" do
+      it 'dont list users tweets after' do
         expect(subject.result.size).to eq(user.tweets.count)
       end
     end
 
-    context "after follow a new user" do
+    context 'after follow a new user' do
       let(:new_user) { create(:user) }
-      it "dont list user tweets after" do
+      it 'dont list user tweets after' do
         create_list(:tweet, 10, user: new_user)
         expect { create(:user_follow, follower: user, following: new_user) }.to change {
           described_class.new(user.id).result.size
@@ -48,10 +48,10 @@ RSpec.describe TweetBusiness::TweetFeed do
     end
   end
 
-  context "when user not found" do
+  context 'when user not found' do
     subject { described_class.new(0) }
 
-    it "raise record not found" do
+    it 'raise record not found' do
       expect { subject }.to raise_exception(ActiveRecord::RecordNotFound)
     end
   end
